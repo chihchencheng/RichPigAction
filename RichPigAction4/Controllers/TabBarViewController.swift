a470b261225bef57175fc955ae16f3daabc411c6
Cheng <c.cheng17783@gmail.com>
2020-07-10 16:32:06 +0800

    20200710 new version of final project

---
+++ RichPigAction4/Controllers/TabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarViewController.swift
+//  RichPigAction4
+//
+//  Created by cheng on 2020/7/10.
+//  Copyright © 2020 cheng. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
---
+++ RichPigAction4/Utils/NetworkController.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkController.swift
+//  RichPigAction4
+//
+//  Created by cheng on 2020/7/10.
+//  Copyright © 2020 cheng. All rights reserved.
+//
+
+import Foundation
